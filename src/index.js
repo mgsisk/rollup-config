a37030bd0a1069526ee555f90aae6c0b1986f820
Michael Sisk <me@mgsisk.com>
2020-04-15 12:14:51 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const babel = require('rollup-plugin-babel')
+const {terser} = require('rollup-plugin-terser')
+
+module.exports = [
+  babel({presets: ['@babel/preset-env']}),
+  terser({compress: {unsafe: true}}),
+]
